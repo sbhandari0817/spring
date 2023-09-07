865072f79f8f47212fd58a770247b24a1ff207f7
sbhandari0817 <santosh.bhandari2@mavs.uta.edu>
2023-09-07 16:43:19 -0500

    Pathvariable with DB

--- src/main/java/com/sb/springboot/demo/springapp/rest/StudentController.java
+++ src/main/java/com/sb/springboot/demo/springapp/rest/StudentController.java
@@ -2,14 +2,12 @@ package com.sb.springboot.demo.springapp.rest;
 
 import com.sb.springboot.demo.springapp.dao.StudentDAO;
 import com.sb.springboot.demo.springapp.entity.Student;
-import com.sb.springboot.demo.springapp.entityclass.StudentE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -28,12 +26,7 @@ public class StudentController {
     }
 
     @GetMapping("/student/{studentId}")
-    public StudentE getStudentWithId(@PathVariable int studentId) {
-        List<StudentE> list = new ArrayList<>();
-        list.add(new StudentE("Ram", "Khadka"));
-        list.add(new StudentE("Hari", "Sharma"));
-
-        return list.get(studentId);
+    public Student getStudentWithId(@PathVariable int studentId) {
+        return studentDAO.findById(studentId);
     }
-
 }
